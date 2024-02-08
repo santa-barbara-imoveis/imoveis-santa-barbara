b2ae310bf234ddb0f2087a6f882099852d6a9e00
unknown <rafaelsanches2002@yahoo.com.br>
2024-02-07 22:40:33 -0300

    abrindo json

---
+++ src/pages/landing-page/dataImoveis.js
@@ -0,0 +1,34 @@
+const imoveis = [
+    {
+        cod: "v035",
+        contrato: "Locação",
+        tipoImovel:"Casa",
+        bairro:"Residencial Santo Antônio",
+        cidade:"Santa_Bárbara",
+        Estado:"Minas_Gerais",
+        quartos:3,
+        banheiros:2,
+        vagas:5,
+        valor:920000,
+        areaTotal:1080,
+        areaConstruida: 301,
+        titulo:"Casa de Alto padrão à venda. Residencial_Santo_Antônio",
+        descricao:"Sala de TV, Sala de Jantar, Cozinha estilo americana, despensa, área de serviço, quintal, garagem ampla"
+    },
+    {
+        cod: "v047",
+        contrato: "Venda",
+        tipoImovel:"Lote",
+        bairro:"Centro",
+        cidade:"Santa_Bárbara",
+        Estado:"Minas_Gerais",
+        quartos:0,
+        banheiros:0,
+        vagas:0,
+        valor:550000,
+        areaTotal:520,
+        areaConstruida: 0,
+        titulo:"Ótima_oportunidade_de_compra - Imóvel_no_centro_de_Santa_Bárbara",
+        descricao:"Imóvel com área total construída de 520 m2 no centrão da cidade, Localizado na rua João Mota, Ideal para fim comercial, 13m de frente, Infraestrutura completa rua calçada água, energia, esgoto e passeio",
+    }
+]

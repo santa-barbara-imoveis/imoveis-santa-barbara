84682f26208995bac07f96683c64f300ead93f20
unknown <rafaelsanches2002@yahoo.com.br>
2024-07-31 15:08:04 -0300

    finalizando videos

--- src/pages/landing-page/components/videoSection/videoSection.js
+++ src/pages/landing-page/components/videoSection/videoSection.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import "./videoSection.css";
+import video1 from "./videos/video1.mp4";
+import video2 from "./videos/video2.mp4";
+import video3 from "./videos/video3.mp4";
+import poster1 from "./videos/capaVideo1.jpg";
+import poster2 from "./videos/capaVideo2.jpg";
+import poster3 from "./videos/capaVideo3.jpg";
+import icon1 from "./videos/casa.png";
+import icon2 from "./videos/chaves.png";
 
 function VideoSection() {
     return (
       <article className='boxVideo'>
-        <div className="imVideo"> 
-          <iframe width="300" height="200" src="https://www.youtube.com/embed/eRpPNIqzvO0?si=CDRAUEXv7pyCAJaS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        </div>
-        <div className="imVideo"> 
-          <iframe width="300" height="200" src="https://www.youtube.com/embed/eRpPNIqzvO0?si=CDRAUEXv7pyCAJaS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        </div>
+        <video className="" src={video1} controls width={200} poster={poster1}> 
+        </video>
+        <img className="iconVideos" src={icon1} />
+        <video className="" src={video2} controls width={200} poster={poster2}> 
+        </video>
+        <img className="iconVideos" src={icon2} />
+        <video className="" src={video3} controls width={200} poster={poster3}> 
+        </video>
       </article>
     );
   }

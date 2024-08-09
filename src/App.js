629ea1ba529be0b75769dae39c167d1862b693d6
unknown <rafaelsanches2002@yahoo.com.br>
2024-08-09 11:21:17 -0300

    instalando react ga4

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import './App.css';
 import "./assets/style/reset.css";
-import ReactGA from "./react-ga";
+import ReactGA from "./react-ga4";
 import { FFooter } from './pages/landing-page/components/Footers/FFooter';
 import { Footers } from './pages/landing-page/components/Footers/Footers';
 import { Header } from './pages/landing-page/components/header/Header';
 
 const TRACKING_ID = "G-FND0LWMLWY";
 ReactGA.initialize(TRACKING_ID);
+
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+})
 
 function App() {
   return (

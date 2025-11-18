43b326af0d81c154b57d0221938154ab61210d7f
unknown <rafaelsanches2002@yahoo.com.br>
2025-11-18 17:52:02 -0300

    atualizados highlights rent

--- src/pages/landing-page/components/highlights rent/highlightsRent.js
+++ src/pages/landing-page/components/highlights rent/highlightsRent.js
@@ -6,8 +6,8 @@ import area from "./assets/area.png";
 import vagas from "./assets/carros.png";
 import quartos from "./assets/quarto.png";
 import fotoFrente4 from "./assets/L073.jpg";
-import fotoFrente5 from "./assets/L065.jpg";
-import fotoFrente6 from "./assets/L100.jpeg";
+import fotoFrente5 from "./assets/L063a1.jpeg";
+import fotoFrente6 from "./assets/L093.jpg";
 import setaE from "./assets/seta-esquerda.png";
 import setaD from "./assets//seta-direita.png";
 
@@ -20,24 +20,24 @@ function HighlightsRent() {
       <div className="opcaoImoveis">
         <div className="boxImovel">
           <div className="imFotoMainR">
-            <Link to={`imovel/L100`} className="linkImage">
+            <Link to={`imovel/L093`} className="linkImage">
               <img className="imFoto2" src={fotoFrente6} />
             </Link>
             <div className="boxPreco">
               <div className="tipoContrato">Locação - Residencial</div>
-              <div className="precoImovel">R$ 2.200,00</div>
+              <div className="precoImovel">R$ 1.800,00</div>
             </div>
           </div>
-          <div className="imBairroCidade">São José - Santa Bárbara</div>
-          <div className="imId">Código: L100</div>
+          <div className="imBairroCidade">Serra Vila - Barão de Cocais</div>
+          <div className="imId">Código: L093</div>
           <div className="imAmenities">
             <div className="iconsAmenities">
               <img className="icon" src={area} />
-              -
+              166 m²
               <img className="icon" src={banho} />
               2
               <img className="icon" src={vagas} />
-              3
+              1
               <img className="icon" src={quartos} />3
             </div>
           </div>
@@ -45,25 +45,25 @@ function HighlightsRent() {
 
         <div className="boxImovel">
           <div className="imFotoMainR">
-            <Link to={`imovel/L065`} className="linkImage">
+            <Link to={`imovel/L063a1`} className="linkImage">
               <img className="imFoto2" src={fotoFrente5} />
             </Link>
             <div className="boxPreco">
               <div className="tipoContrato">Locação</div>
-              <div className="precoImovel">R$ 1.500,00</div>
+              <div className="precoImovel">R$ 2.200,00</div>
             </div>
           </div>
-          <div className="imBairroCidade">Santo Antônio - Santa Bárbara</div>
-          <div className="imId">Código: L065</div>
+          <div className="imBairroCidade">Centro - Santa Bárbara</div>
+          <div className="imId">Código: L063a1</div>
           <div className="imAmenities">
             <div className="iconsAmenities">
               <img className="icon" src={area} />
-              100
+              -
               <img className="icon" src={banho} />
-              2
-              <img className="icon" src={vagas} />
               1
-              <img className="icon" src={quartos} />2
+              <img className="icon" src={vagas} />
+              0
+              <img className="icon" src={quartos} />0
             </div>
           </div>
         </div>

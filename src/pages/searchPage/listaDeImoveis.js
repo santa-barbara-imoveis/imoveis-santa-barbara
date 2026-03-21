27eeeb30831a4ee251e49bac1403b93f6b0d95c1
rsanches2002 <rafaelsanches2002@yahoo.com.br>
2026-03-21 10:55:15 -0300

    buscando solucoes problema renderizacao

--- src/pages/searchPage/listaDeImoveis.js
+++ src/pages/searchPage/listaDeImoveis.js
@@ -10,36 +10,35 @@ import quartos from "../../assets/icons/quarto.png";
 import tpImovel from "../../assets/icons/tipoImovel.png";
 import { Link } from "react-router-dom";
 
-function RenderImoveis({itens}) 
-{
-    return (
-      <div className="carrosel">
-        {itens.map((item) => (
-        <div className="BoxImovel">
+function RenderImoveis({ itens }) {
+  return (
+    <div className="carrosel">
+      {itens.map((item) => (
+        <div key={item.cod} className="BoxImovel">
           <div className="boxPicture">
             <Link to={`imovel/${item.cod}#goTitle`} className='linkImage'>
-              <img className="imFoto" src={item.fotoCapa}/>
+              <img className="imFoto" src={item.fotoCapa} />
             </Link>
           </div>
           <div className="boxDataImovel">
             <div className="boxTitulo"> {item.bairro} | {item.cidade} | {item.cod} </div>
             <div className="boxAmenitiesData">
-              <div className='ltBoxData'> <img className="iconS" src={iconContrato}/> {item.contrato} </div>
-              <div className='ltBoxData'> <img className="iconS" src={tpImovel}/> {item.tipoImovel} </div>
-              <div className='ltBoxData'>                      
-                <img className="iconS" src={iconMoeda}/> {item.valor} </div>
-              <div className='ltBoxData'>                    
-                <img className="iconS" src={iconTotal}/>Área total: {item.areaTotal} </div>
-              <div className='ltBoxData'> <img className="iconS" src={area}/> Área construida: {item.areaConstruida} </div>
-              <div className='ltBoxData'> <img className="iconS" src={banho}/> {item.banheiros} banheiro/s </div>
-              <div className='ltBoxData'> <img className="iconS" src={quartos}/> {item.quartos} quartos </div>
-              <div className='ltBoxData'> <img className="iconS" src={vagas}/> {item.vagas} vagas </div>
+              <div className='ltBoxData'> <img className="iconS" src={iconContrato} /> {item.contrato} </div>
+              <div className='ltBoxData'> <img className="iconS" src={tpImovel} /> {item.tipoImovel} </div>
+              <div className='ltBoxData'>
+                <img className="iconS" src={iconMoeda} /> {item.valor} </div>
+              <div className='ltBoxData'>
+                <img className="iconS" src={iconTotal} />Área total: {item.areaTotal} </div>
+              <div className='ltBoxData'> <img className="iconS" src={area} /> Área construida: {item.areaConstruida} </div>
+              <div className='ltBoxData'> <img className="iconS" src={banho} /> {item.banheiros} banheiro/s </div>
+              <div className='ltBoxData'> <img className="iconS" src={quartos} /> {item.quartos} quartos </div>
+              <div className='ltBoxData'> <img className="iconS" src={vagas} /> {item.vagas} vagas </div>
             </div>
           </div>
         </div>
-        ))}
-      </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default RenderImoveis
